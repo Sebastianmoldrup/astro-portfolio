3b2b467271a2eb710a1e9164c2127a21a198080c
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2023-01-26 17:37:03 +0100

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+
+export default function Menu() {
+
+    const anchor = "flex justify-center items-center text-xl opacity-75 hover:opacity-100 ease-in-out duration-200 hover:scale-125 hover:border-b-2";
+    
+    return (
+        <div class="flex flex-row justify-between w-full px-14 pt-6 text-[#34e4b1] overflow-hidden">
+
+            {/* Name */}
+            <div class="font-bold text-2xl">Sebastian Møldrup</div>
+
+            {/* Nav - desktop */}
+            <nav class="hidden lg:flex justify-between items-center w-[500px] h-[40px]">
+                <a href="" class={anchor}>Hjem</a>
+                <a href="" class={anchor}>Om meg</a>
+                <a href="" class={anchor}>Prosjekter</a>
+                <a href="" class={anchor}>Kontakt</a>
+            </nav>
+
+            {/* Burger menu */}
+            <div class="flex lg:hidden justify-center p-0 m-0">
+                <a class="">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30px" 
+                        height="30px" 
+                        viewBox="0 0 16 16"
+                    >
+                    <path 
+                        fill="none" 
+                        stroke="currentColor" 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        stroke-width="1.5" 
+                        d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"/
+                    >
+                    </svg>
+                </a>
+            </div>
+
+            {/* Nav - mobile */}
+            <div class="hidden">
+                <nav class="hidden lg:block">
+                    <a href="">Hjem</a>
+                    <a href="">Om meg</a>
+                    <a href="">Prosjekter</a>
+                    <a href="">Kontakt</a>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
