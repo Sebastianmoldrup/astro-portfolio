5adc4d18f2ea908dc7ff9bb58a379cba2268fc2b
Sebastianmoldrup <Sebastian.moldrup@gmail.com>
2023-01-31 18:13:34 +0100

    mobile testing

--- src/components/header/NavMobile.jsx
+++ src/components/header/NavMobile.jsx
@@ -1,29 +1,16 @@
 import { signal } from "@preact/signals";
 import { getCollection } from 'astro:content';
+import { Burger, Close } from '../../images/icons/Svg.jsx';
 const mdx = await getCollection('blog');
 
 const state = signal(0);
 
-export function Burger() {
+export function Menu() {
 
     return (
         <div class="flex lg:hidden justify-center items-center p-0 m-0 cursor-pointer" onClick={() => state.value ? state.value-- : state.value++}>
             <a class="w-[30px] h-[30px] pt-0.5">
-                <svg 
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30px" 
-                    height="30px" 
-                    viewBox="0 0 16 16"
-                >
-                <path 
-                    fill="none" 
-                    stroke="currentColor" 
-                    stroke-linecap="round" 
-                    stroke-linejoin="round" 
-                    stroke-width="1.5" 
-                    d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"/
-                >
-                </svg>
+                <Burger />
             </a>
         </div>
     );
@@ -34,7 +21,7 @@ export function Overlay() {
     const val = state.value ? "block" : "hidden";
 
     return (
-        <div class={"fixed left-0 grid grid-rows-4 w-screen h-screen p-0 m-0 top-14 bg-[#101115] text-[#34e4b1] lg:hidden " + val}>
+        <div class={"fixed left-0 grid grid-rows-4 w-screen h-screen p-0 m-0 bg-[#101115] text-[#34e4b1] lg:hidden " + val}>
             <nav class={"flex flex-col row-start-2 text-center text-2xl gap-5 mb-5"}>
                 { mdx.sort( (a, b) => {
                     return (a.data.position || 0) - (b.data.position || 0);
--- src/images/icons/Svg.jsx
+++ src/images/icons/Svg.jsx
@@ -74,3 +74,35 @@ export function Person() {
         </svg>
     );
 }
+
+export function Burger() {
+    return (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            width="30px" 
+            height="30px" 
+            viewBox="0 0 16 16">
+        <path 
+            fill="none" 
+            stroke="currentColor" 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            stroke-width="1.5" 
+            d="M2.75 12.25h10.5m-10.5-4h10.5m-10.5-4h10.5"/>
+        </svg>
+    );
+}
+
+export function Close() {
+    return (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="30px" 
+            height="30px" 
+            viewBox="0 0 24 24">
+        <path 
+            fill="#34e4b1" 
+            d="M6.4 19L5 17.6l5.6-5.6L5 6.4L6.4 5l5.6 5.6L17.6 5L19 6.4L13.4 12l5.6 5.6l-1.4 1.4l-5.6-5.6Z"/>
+        </svg>
+    );
+}
